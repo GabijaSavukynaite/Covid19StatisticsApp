a026e8a860bf0a656d38c0a07c876ac683d26193
Gabija <gabija.savu@gmail.com>
2021-02-01 00:22:12 +0200

    Added rest api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,35 @@
+import axios from "axios";
 import express from "express";
+import { CovidData } from "./types";
 
 const PORT = 8080;
 
 const app = express();
+
+let covidData: CovidData[] = [];
+
+app.use(express.json());
+
+app.get("/covidData", (_, res) => {
+  axios
+    .get("https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/json")
+    .then((response) => {
+      covidData = response.data;
+    })
+    .catch((error) => {
+      console.log("Error: " + error.code);
+    })
+    .then(() => res.send(covidData.length.toString()));
+});
+
+app.get("/covidData/:country", (req, res) => {
+  const country = req.params.country;
+
+  const filteredByCountry = covidData.filter(
+    (data) => data.country === country
+  );
+
+  res.send(filteredByCountry);
+});
 
 app.listen(PORT);
---
+++ server/src/types.ts
@@ -0,0 +1,11 @@
+export type CovidData = {
+  country: string;
+  country_code: string;
+  continent: string;
+  population: number;
+  indicator: string;
+  weekly_count: number;
+  year_week: string;
+  cumulative_count: number;
+  source: string;
+};
